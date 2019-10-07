1b57ae30cfefdabe6cc9ef22fbddee29906c339f
Aaron Davis <adbe455@gmail.com>
2019-10-07 16:23:44 -0400

    ad

---
+++ src/app/modules/home/splash/splash.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { GameSearch } from '../../../game-search.service'
+
+@Component({
+  selector: 'app-splash',
+  templateUrl: './splash.component.html',
+  styleUrls: ['./splash.component.css']
+})
+export class SplashComponent implements OnInit {
+
+  public results: any = [];
+  public searching: any = false;
+
+  constructor(private _gameSearch: GameSearch) {}
+
+  toggleSearching(){
+    this.searching = !this.searching;
+  }
+
+  search(query){
+    this.toggleSearching();
+    this._gameSearch.fetch(query)
+      .subscribe(data => {this.results = data; console.log(data); this.toggleSearching()})
+  }
+
+  ngOnInit() {
+  }
+
+}
