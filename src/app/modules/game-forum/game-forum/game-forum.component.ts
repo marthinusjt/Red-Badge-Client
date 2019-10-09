3a633e6c03da458612f9dbed5c411a8a1f9993a3
Aaron Davis <adbe455@gmail.com>
2019-10-09 11:13:38 -0400

    ad

---
+++ src/app/modules/game-forum/game-forum/game-forum.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-forum',
+  templateUrl: './game-forum.component.html',
+  styleUrls: ['./game-forum.component.css']
+})
+export class GameForumComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
