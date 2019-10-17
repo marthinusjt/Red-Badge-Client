c12d64448d1f47b4f06e417afb33c32b321e2e2b
marthinusjt <marthinusjt@gmail.com>
2019-10-17 12:27:00 -0400

    mjt

--- src/app/modules/game-forum/game-forum/game-forum.service.ts
+++ src/app/modules/game-forum/game-forum/game-forum.service.ts
@@ -15,6 +15,8 @@ export class GameForumService {
     private _proxy: string = 'https://cors-anywhere.herokuapp.com/';
     
     private _url2: string = 'http://localhost:3343/forumTopic/';
+
+    private _url3: string = 'http://localhost:3343/forumReply/';
     
     // public query: string;
     
@@ -22,9 +24,44 @@ export class GameForumService {
         private http: HttpClient,
     ) { }
 
+    forumReplyGetAll(query, category, topicId) {
+        return this.http.get(this._url3 + `all/${query}/${category}/${topicId}`)
+    }
+
+    // SINGULAR
+    // forumTopicGet(query, category, topicId) {
+    //     return this.http.get(this._url2 + `${query}/${category}/${topicId}`)
+    //     //data,this._proxy +     
+    // }
+
+    // ALL
     forumTopicGetAll(query, category) {
-        return this.http.get(this._url2 +`all/${query}/${category}`)
+        return this.http.get(this._url2 + `all/${query}/${category}`)
         //data,this._proxy +     
-      }
+    }
+
+    forumTopicPost(query: number, category: string, pinned: boolean, textArea: string, topic: string) {
+
+        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        let token = currentUser.token;
+
+        let body = {
+
+            pinned: pinned,
+            topic: topic,
+            textArea: textArea
+            
+        }
+
+        const parseHeaders = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json',
+                'Authorization': token.sessionToken
+            })
+        };
+
+        return this.http.post(this._url2 +`${query}/${category}`, body, parseHeaders)
+        
+    }
     
 }
