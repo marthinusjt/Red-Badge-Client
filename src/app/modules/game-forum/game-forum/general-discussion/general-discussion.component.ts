2672baad35be252024d4c750c29b6aaefad71dea
Aaron Davis <adbe455@gmail.com>
2019-10-21 11:02:16 -0400

    ad

--- src/app/modules/game-forum/game-forum/general-discussion/general-discussion.component.ts
+++ src/app/modules/game-forum/game-forum/general-discussion/general-discussion.component.ts
@@ -44,6 +44,12 @@ export class GeneralDiscussionComponent implements OnInit {
       .subscribe(data => {
         this.results = data; 
         this.results.reverse();
+        this.results.forEach((e, i) => {
+          if(e.pinned){
+            this.results.splice(i, 1);
+            this.results.unshift(e);
+          }
+        })
         console.log(this.results);
       })
   }
